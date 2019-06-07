48102db1cfa2d276e7971063cbd86ac3ed79aac0
ForgeMaster132 <dayleklinkhamer@gmail.com>
2019-06-08 01:36:26 +0200

    final

--- script.js
+++ script.js
@@ -1,80 +1,92 @@
 // Functions
 
-// Math Functions
-
 function add(num1,num2){
     let answer = parseInt(num1) + parseInt(num2);
     return answer;
 }
 
 function subtract(num1,num2){
-    let answer = num1 - num2;
+    let answer = parseInt(num1) - parseInt(num2);
     return answer;
 }
 
 function multiply(num1,num2){
-    let answer = num1 * num2;
+    let answer = parseInt(num1) * parseInt(num2);
     return answer;
 }
 
 function divide(num1,num2){
-    let answer = num1 / num2;
+    let answer = parseInt(num1) / parseInt(num2);
     return answer;
+}
+
+function firstNumBig(numberSize,problem,i){
+    let size = 0;
+                let j = numberSize;
+                while(j > 0){
+                    size += problem[i - j];
+                    j--;
+                }
+    return size;
+}
+
+function nextNumBigCheck(problem,i){
+    let count = 0;
+    let k = 1;
+    for(let j = 0;j < problem.length - (i + 1); j++){
+        if(problem[i + k] != "+" || "-" || "/" || "x")
+        count++;
+    }
+    return count;
+}
+
+function nextNumBig(problem,i,count){
+    let size = 0;
+                let j = count;
+                let k = 1;
+                while(j > 0){
+                    size += problem[i + k];
+                    j--;
+                    k++;
+                }
+    return size;
 }
 
 function operate(){
     let problem = document.getElementById("display").textContent.split("");
     let answer = 0;
     let numberSize = 0;
-    for(let i = 0;i < problem.length;i++){
+    let num2 = 0;
+    let size = 0;
+    let size2 = 0;
+    for(let i = 0;i < problem.length;i++){  
+        size = problem[i - 1];
+        size2 = problem[i + 1];
+        num2 = nextNumBigCheck(problem,i);
        if(problem[i] == "+"){
-            if(numberSize > 1){
-                let size = 0;
-                let j = numberSize;
-                while(j > 0){
-                    size += problem[i - j];
-                    j--;
-                }
-                answer = add(size,problem[i + 1])
-            }else
-                answer = add(problem[i - 1],problem[i + 1]);
-                problem.splice(i+1,1,answer);
-        }else if (problem[i] == "-"){
-            if(numberSize > 1){
-                let size = 0;
-                let j = numberSize;
-                while(j > 0){
-                    size += problem[i - j];
-                    j--;
-                }
-                answer = subtract(size,problem[i + 1])
-            }else
-            answer = subtract(problem[i - 1],problem[i + 1]);
-            problem.splice(i+1,1,answer);
-        } else if (problem[i] == "*"){
-            if(numberSize > 1){
-                let size = 0;
-                let j = numberSize;
-                while(j > 0){
-                    size += problem[i - j];
-                    j--;
-                }
-                answer = multiply(size,problem[i + 1])
-            }else
-            answer = multiply(problem[i - 1],problem[i + 1]);
-            problem.splice(i+1,1,answer);
-        } else if (problem[i] == "/"){
-            if(numberSize > 1){
-                let size = 0;
-                let j = numberSize;
-                while(j > 0){
-                    size += problem[i - j];
-                    j--;
-                }
-                answer = divide(size,problem[i + 1])
-            }else
-            answer = divide(problem[i - 1],problem[i + 1]);
-            problem.splice(i+1,1,answer);
+                size2 = nextNumBig(problem,i,num2);
+                size = firstNumBig(numberSize,problem,i);
+                    answer += add(size,size2);
+                    problem.splice(i + 1,1,answer);
+                    numberSize = 0;
+        } else if(problem[i] == "-"){
+                size2 = nextNumBig(problem,i,num2);
+                size = firstNumBig(numberSize,problem,i);
+                    answer += subtract(size,size2);
+                    problem.splice(i + 1,1,answer);
+                    numberSize = 0;
+        } else if(problem[i] == "x"){
+                size2 = nextNumBig(problem,i,num2);
+                size = firstNumBig(numberSize,problem,i);
+                    answer += multiply(size,size2);
+                    problem.splice(i + 1,1,answer);
+                    numberSize = 0;
+        } else if(problem[i] == "/"){
+                size2 = nextNumBig(problem,i,num2);
+                size = firstNumBig(numberSize,problem,i);
+                    answer += divide(size,size2);
+                    problem.splice(i + 1,1,answer);
+                    numberSize = 0;
         }
       numberSize++;
     }
@@ -100,7 +112,7 @@ for(let i = 0; i < 10; i++){
     });
 }
 
-let operators= ["+","-","*","/"];
+let operators= ["+","-","x","/"];
 for(let i = 0; operators.length > i; i++){
     document.getElementById(operators[i]).addEventListener("click",function(){
         if(document.getElementById("display").textContent.length > 12){
@@ -119,8 +131,9 @@ for(let i = 0; operators.length > i; i++){
 
 document.getElementById("=").addEventListener("click",operate);
 
-document.getElementById("clear").addEventListener("click",function(){
+    document.getElementById("clear").addEventListener("click",function(){
     document.getElementById("display").style.fontSize = "300%";
     document.getElementById("display").style.height = "10%";
     document.getElementById("display").textContent = "Calculator";
 });
+
